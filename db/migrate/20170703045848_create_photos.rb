047ae35e1edd61905b160cc04d2711c8c32f0199
Anh Viet <virt@vinova.sg>
2017-08-18 22:56:23 +0700

    f

--- db/migrate/20170703045848_create_photos.rb
+++ db/migrate/20170703045848_create_photos.rb
@@ -2,8 +2,6 @@ class CreatePhotos < ActiveRecord::Migration[5.0]
   def change
     create_table :photos do |t|
     	t.string :title
-    	t.string :gallary_id
-    	t.string :gallary_token
     	t.string :image
       t.timestamps
     end
